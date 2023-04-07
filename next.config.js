f49641d54b19c8260b3aa9a6217c6c8b0f6ba210
Aihara <aihara-sama@mail.ru>
2023-04-07 18:36:05 +0300

    feat(env): added env firebase

--- next.config.js
+++ next.config.js
@@ -9,4 +9,13 @@ const withBundleAnalyzer = bundleAnalyzer({
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   i18n,
+  env: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH_OMAIN: process.env.FIREBASE_AUTH_OMAIN,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+  },
 });
--- src/configs/index.ts
+++ src/configs/index.ts
@@ -4,13 +4,13 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDREk2iHr_2SKp7knLeiPsF6tyg5WC9ric",
-  authDomain: "show-now-524c1.firebaseapp.com",
-  projectId: "show-now-524c1",
-  storageBucket: "show-now-524c1.appspot.com",
-  messagingSenderId: "584535450899",
-  appId: "1:584535450899:web:789a099d8ecd109667a6e6",
-  measurementId: "G-Q2EDHVK16X",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_OMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 export { firebaseConfig };
