b5e15db50415b5b860b4e2e97596c88972b690fa
Aihara <aihara-sama@mail.ru>
2023-04-09 12:41:59 +0300

    feat(upload-images-check): added check for file type

--- src/components/common/buttons/UploadButton/index.tsx
+++ src/components/common/buttons/UploadButton/index.tsx
@@ -2,6 +2,7 @@ import { Box, Button, CircularProgress } from "@mui/material";
 import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 import type { ChangeEvent, FunctionComponent } from "react";
 import { useEffect, useRef, useState } from "react";
+import { toast } from "react-hot-toast";
 import { v4 as uuid } from "uuid";
 
 interface IProps {
@@ -28,15 +29,19 @@ const UploadButton: FunctionComponent<IProps> = ({
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files[0];
     if (file) {
-      const storage = getStorage();
-      const storageRef = ref(storage, uuid());
-      setIsUploading(true);
-      uploadBytes(storageRef, file).then(() => {
-        getDownloadURL(storageRef).then((url) => {
-          setImageUrl(url);
-          setIsUploading(false);
+      if (!/^image\/(png|jpe?g)$/.test(file.type)) {
+        toast.error("Please provide only PNG or JPG/JPEG files");
+      } else {
+        const storage = getStorage();
+        const storageRef = ref(storage, uuid());
+        setIsUploading(true);
+        uploadBytes(storageRef, file).then(() => {
+          getDownloadURL(storageRef).then((url) => {
+            setImageUrl(url);
+            setIsUploading(false);
+          });
         });
-      });
+      }
     }
   };
 
